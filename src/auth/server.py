8899cf9b84e0444119eeab7a5dc381ee05d6d0db
Richard Donald <richard_donald92@outlook.com>
2024-11-05 08:19:47 -0500

    JWT validation

--- src/auth/server.py
+++ src/auth/server.py
@@ -1,5 +1,9 @@
-import jwt, datetime, os
+import datetime
+import os
+import jwt
+
 from flask import Flask, request
+from flask_mysqldb import MySQL
 
 server = Flask(__name__)
 mysql = MySQL(server)
@@ -11,6 +15,7 @@ server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
 server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
 server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
 
+
 @server.route("/login", methods=["POST"])
 def login():
     auth = request.authorization
@@ -18,4 +23,41 @@ def login():
         return "missing credentials", 401
     
     # check db for username and password
-    
+    cur = mysql.connection.cursor()
+    res = cur.execute(
+        f"SELECT email, password FROM user WHERE email={(auth.username,)}"
+    )
+    if len(res) > 0:
+        user_row = cur.fetchone()
+        email, password = user_row
+
+        if auth.username != email or auth.password != password:
+            return "invalid credentials", 401
+        return create_jwt(auth.username, os.environ.get("JWT_SECRET"), True)
+
+    return "invalid credentials", 401
+
+
+@server.route("/validate", method=["POST"])
+def validate():
+    encoded_jwt = request.headers["Authorization"]
+    if not encoded_jwt:
+        return "missing credentials", 401
+
+    encoded_jwt = encoded_jwt.split()[1]
+    try:
+        decoded = jwt.decode(encoded_jwt, os.environ.get("JWT_SECRET"), algorithms=["HS256"])
+    except:
+        return "not authorized", 403
+    return decoded, 200
+
+
+def create_jwt(username, secret, authz):
+    return jwt.encode(
+        {"username": username, "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(days=1),
+         "iat": datetime.datetime.now(), "admin": authz}, secret, algorithm="HS256"
+    )
+
+
+if __name__ == "__main__":
+    server.run(port=5000, host="0.0.0.0")
