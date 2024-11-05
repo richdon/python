74848f7a11ae0c215e1ff3645102b7a9e741342c
Richard Donald <richard_donald92@outlook.com>
2024-11-05 08:18:38 -0500

    update db

--- src/auth/init.sql
+++ src/auth/init.sql
@@ -1,15 +1,15 @@
-CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Aauth123'
+CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Aauth123';
 
 CREATE DATABASE auth;
 
-GRANT ALL PRIVILEGES auth.* TO 'auth_user'@'localhost';
+GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
 
-USE auth
+USE auth;
 
 CREATE TABLE user (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
 
-INSERT INTO USER (email, password) VALUES ('admin@email.com', 'Admin123')
+INSERT INTO USER (email, password) VALUES ('admin@email.com', 'Admin123');
